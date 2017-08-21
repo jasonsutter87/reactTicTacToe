37256595266f93ed3686818216b79748469cacc0
Jason Sutter <jasonsutter87@gmail.com>
2017-08-21 16:58:28 -0500

    Implement a no winner message

--- src/index.js
+++ src/index.js
@@ -42,6 +42,35 @@ class Board extends React.Component {
     );
   }
 }
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  let count = 0;
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  let result = (squares.every(function(i){
+    return i != null;
+  }));
+
+  if(result) {
+    return "No Winner";
+  }
+  
+  return null;
+}
+
 
 class Game extends React.Component {
   constructor() {
@@ -103,6 +132,7 @@ class Game extends React.Component {
     } else {
       status = "Next player: " + (this.state.xIsNext ? "🤖" : "👾");
     }
+    // TODO cats game option
 
     return (
       <div className="game">
@@ -124,23 +154,3 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
